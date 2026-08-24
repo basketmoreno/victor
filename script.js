ab8f82760758ae8b167d3e141fe61ebe199eb848
Victor Moreno <53240782+basketmoreno@users.noreply.github.com>
2026-08-24 16:15:43 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Abrir y cerrar la barra lateral en móviles
-document.querySelector('.mobile-menu-icon').addEventListener('click', function() {
-    document.querySelector('.mobile-sidebar').style.width = '250px';
-});
-
-document.querySelector('.mobile-sidebar .close-btn').addEventListener('click', function() {
-    document.querySelector('.mobile-sidebar').style.width = '0';
-});
